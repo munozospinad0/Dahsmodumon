f0a02a858e771baf9c681538b5f796e1909b28bc
munozospinad0 <munozospinad0@gmail.com>
2026-07-03 11:33:36 -0500

    Conecta config.js con la URL del Apps Script (dashboard en vivo)

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // 1) Despliega el Apps Script como Web App y pega aquí su URL /exec
 // 2) KEY debe ser IGUAL a la del Codigo.gs
 window.DASH = {
-  API: '',                 // <-- PEGA AQUÍ la URL del Apps Script (…/exec)
+  API: 'https://script.google.com/macros/s/AKfycbzJeKpyfLZw1KMCJoDoQOyONJMNA6aonGUYgKTJOqeOyF3xGLC0tSZX7iY1Omoycl_B/exec',
   KEY: 'modumon2026',      // misma clave que en Codigo.gs
   REFRESH_MS: 90000        // auto-refresco cada 90s
 };
